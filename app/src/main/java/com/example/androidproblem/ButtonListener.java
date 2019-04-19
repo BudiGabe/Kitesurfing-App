7575cc8249b8881af9c88b2dd747d331d925d075
Razoku <razvan.gabriel.budaca@gmail.com>
2019-04-19 18:23:09 +0300

    Moved onClickListener to separate class, added more comments

--- app/src/main/java/com/example/androidproblem/ButtonListener.java
+++ app/src/main/java/com/example/androidproblem/ButtonListener.java
@@ -1,13 +1,62 @@
 package com.example.androidproblem;
 
+import android.graphics.drawable.Drawable;
 import android.view.View;
+
 
 import data.model.AddFavPOST;
 import data.model.RemoveFavPOST;
+import data.remote.APIService;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ButtonListener{
+public class ButtonListener implements View.OnClickListener{
+    String token;
+    APIService mApiService;
+    Drawable star;
+    Drawable starOn;
+    Drawable starOff;
+    public ButtonListener(String token, APIService mApiService, Drawable starOn, Drawable starOff){
+        this.token = token;
+        this.mApiService = mApiService;
+        this.starOn = starOn;
+        this.starOff = starOff;
+    }
 
+    @Override
+    public void onClick(final View v) {
+        //get id of spot and favorite status
+        String spotIdCurrent = v.getTag().toString();
+        star = v.getBackground();
+        //compare to see if favorited using the drawables
+        if(star.getConstantState().equals(starOn.getConstantState())){
+            mApiService.remSpotFav(token, spotIdCurrent).enqueue(new Callback<RemoveFavPOST>() {
+                @Override
+                public void onResponse(Call<RemoveFavPOST> call, Response<RemoveFavPOST> response) {
+                    //backend wil unfavorite by itself, i just set image resource
+                    v.setBackground(starOff);
+                }
+
+                @Override
+                public void onFailure(Call<RemoveFavPOST> call, Throwable t) {
+
+                }
+            });
+        }
+        else{
+            mApiService.addSpotFav(token, spotIdCurrent).enqueue(new Callback<AddFavPOST>() {
+                @Override
+                public void onResponse(Call<AddFavPOST> call, Response<AddFavPOST> response) {
+                    //backend will favorite by itself, i just set image resource
+                    v.setBackground(starOn);
+                }
+
+                @Override
+                public void onFailure(Call<AddFavPOST> call, Throwable t) {
+
+                }
+            });
+        }
+    }
 }
--- app/src/main/java/com/example/androidproblem/MainActivity.java
+++ app/src/main/java/com/example/androidproblem/MainActivity.java
@@ -3,23 +3,16 @@ package com.example.androidproblem;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import data.model.AddFavPOST;
 import data.model.GetAllSpotsPOST;
 import data.model.GetAllSpotsResult;
-import data.model.RemoveFavPOST;
 import data.model.TokenPost;
 import data.remote.APIService;
 import data.remote.ApiUtils;
@@ -30,48 +23,53 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ScrollView sv;
     private TableLayout tableLayout;
     ImageButton favButton;
     private APIService mApiService;
     static String EMAIL_VALID = "t1@gmail.com";
     String token;
-    Drawable star;
     Drawable starOn;
     Drawable starOff;
+    LinearLayout.LayoutParams lp;
+    List<GetAllSpotsResult> spotList;
+    ButtonListener buttonListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sv = (ScrollView) findViewById(R.id.sv);
         tableLayout = (TableLayout) findViewById(R.id.tableLayout);
 
         mApiService = ApiUtils.getAPIService();
         starOn = getDrawable(R.drawable.star_on);
         starOff = getDrawable(R.drawable.star_off);
 
-        //get token
+        buttonListener = new ButtonListener(token,mApiService,starOn,starOff);
+        lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        //get token from backend
         mApiService.getToken(EMAIL_VALID).enqueue(new Callback<TokenPost>() {
             @Override
             public void onResponse(Call<TokenPost> call, Response<TokenPost> response) {
                 token = response.body().getResult().getToken();
-                //get spot list
+                //get spot list from backend, after getting token
                 mApiService.getAllSpots(token,"","").enqueue(new Callback<GetAllSpotsPOST>() {
                     @Override
                     public void onResponse(Call<GetAllSpotsPOST> call, Response<GetAllSpotsPOST> response) {
-                        List<GetAllSpotsResult> spotList = response.body().getResult();
-                        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        spotList = response.body().getResult();
                         for(int i = 0; i < spotList.size(); i++){
+                            //initialize all needed objects
                             TableRow row = new TableRow(getApplicationContext());
                             LinearLayout textLayout = new LinearLayout(getApplicationContext());
                             LinearLayout buttonLayout = new LinearLayout(getApplicationContext());
                             textLayout.setOrientation(LinearLayout.VERTICAL);
+                            //get all needed spot information
                             GetAllSpotsResult spotCurrent = spotList.get(i);
                             String spotName = spotCurrent.getName();
                             String spotCountry = spotCurrent.getCountry();
                             String spotId = spotCurrent.getId();
+                            //add spot information to row
                             addSpotText(spotName, textLayout, lp);
                             addSpotText(spotCountry, textLayout, lp);
                             addSpotText(spotId, textLayout, lp);
@@ -88,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-
             @Override
             public void onFailure(Call<TokenPost> call, Throwable t) {
             }
@@ -105,52 +102,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addFavButton(LinearLayout.LayoutParams lp, GetAllSpotsResult spotCurrent, LinearLayout buttonLayout){
+        //create every button
         favButton = new ImageButton(getApplicationContext());
         favButton.setLayoutParams(lp);
+        //set tag for easier access to spotId
         favButton.setTag(spotCurrent.getId());
+        //set button image
         if(spotCurrent.getIsFavorite()){
             favButton.setBackground(getDrawable(R.drawable.star_on));
         }
         else{
             favButton.setBackground(getDrawable(R.drawable.star_off));
         }
-        favButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                //get id of spot and favorite status
-                String spotIdCurrent = v.getTag().toString();
-                star = v.getBackground();
-                //compare to see if favorited using the drawables
-                if(star.getConstantState().equals(starOn.getConstantState())){
-                    mApiService.remSpotFav(token, spotIdCurrent).enqueue(new Callback<RemoveFavPOST>() {
-                        @Override
-                        public void onResponse(Call<RemoveFavPOST> call, Response<RemoveFavPOST> response) {
-                            //backend wil unfavorite by itself, i just set image resource
-                            v.setBackground(getDrawable(R.drawable.star_off));
-                        }
-
-                        @Override
-                        public void onFailure(Call<RemoveFavPOST> call, Throwable t) {
-
-                        }
-                    });
-                }
-                else{
-                    mApiService.addSpotFav(token, spotIdCurrent).enqueue(new Callback<AddFavPOST>() {
-                        @Override
-                        public void onResponse(Call<AddFavPOST> call, Response<AddFavPOST> response) {
-                            //backend will favorite by itself, i just set image resource
-                            v.setBackground(getDrawable(R.drawable.star_on));
-                        }
-
-                        @Override
-                        public void onFailure(Call<AddFavPOST> call, Throwable t) {
-
-                        }
-                    });
-                }
-            }
-        });
+        favButton.setOnClickListener(buttonListener);
         buttonLayout.addView(favButton);
     }
 
